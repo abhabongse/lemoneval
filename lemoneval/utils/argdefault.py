4c7a35d39ca0003b891ff2d772795429a3c65675
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2017-11-29 13:17:47 +0700

    Add argdefault for empty function argument as default value

---
+++ lemoneval/utils/argdefault.py
@@ -0,0 +1,10 @@
+# Lemoneval Project
+# Author: Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
+"""Extension of NoneType for function argument placeholder."""
+
+class ArgumentDefault():
+    """Object to represent empty argument default which evaluates to False."""
+
+    def __bool__(self):
+        return False
+    
